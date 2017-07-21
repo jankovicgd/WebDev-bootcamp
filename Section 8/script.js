c569f18bb6698d19b0272145d4de9efa9343f509
jankovicgd <jankovic.gd@gmail.com>
2017-07-21 15:39:03 +0200

    Excercises in javascript

---
+++ Section 8/script.js
@@ -0,0 +1,3 @@
+var userName = prompt("hello from js. Whats your name");
+alert("nice to meet you " + userName);
+console.log("hello, " + userName);
---
+++ Section 8/stalker.js
@@ -0,0 +1,6 @@
+name = prompt("What is your name?");
+surname = prompt("What is your last name?");
+age = prompt("How old are you?")
+
+console.log("Your full name is " + name + " " + surname);
+console.log("You are " + age + " years old")
