584394bc8b958907efa1e48d2489893b8333bff4
jankovicgd <jankovic.gd@gmail.com>
2017-07-22 18:54:27 +0200

    problem set practise

---
+++ Section 9/pset1.js
@@ -0,0 +1,27 @@
+a1 = -10
+while(a1 <= 19){
+  console.log(a1);
+  a1++;
+}
+
+a2 = 10
+while(a2 <= 40){
+  console.log(a2);
+  a2 = a2 + 2;
+}
+
+a3 = 300
+while(a3 <= 333){
+  if(a3 % 2 == 1){
+    console.log(a3);
+  }
+  a3++;
+}
+
+a4 = 5
+while(a4 <= 50){
+  if(a4 % 3 == 0 && a4 % 5 == 0){
+    console.log(a4);
+  }
+  a4++;
+}
