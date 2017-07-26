26893e5dc8b5277cdf3feb08f9d98c5cce78bb59
jankovicgd <jankovic.gd@gmail.com>
2017-07-26 17:54:39 +0200

    excercise

---
+++ Section 13/excercise.js
@@ -0,0 +1,17 @@
+p1 = document.getElementById("first");
+console.log(p1);
+
+p2 = document.querySelector("p")
+console.log(p2);
+
+p3 = document.querySelector(".special");
+console.log(p3);
+
+p4 = document.querySelector("#first");
+console.log(p4);
+
+p5 = document.getElementsByClassName("special")[0];
+console.log(p5);
+
+p6 = document.getElementsByTagName("p")[0];
+console.log(p6);
