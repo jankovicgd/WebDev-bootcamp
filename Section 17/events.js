4f1aea78ecfc51268291b594efaa14ebb9676350
jankovicgd <jankovic.gd@gmail.com>
2017-08-01 11:31:30 +0200

    events all

--- Section 17/events.js
+++ Section 17/events.js
@@ -15,9 +15,27 @@ $('button').click(function(){
 });
 
 $('input').keypress(function(event) {
-  //console.log("you pressed a key");
-  //console.log(event);
-  if (event.which == 13) {
+  // console.log("you pressed a key");
+  // console.log(event);
+  if (event.which == 13) {// if enter is pressed Enter == 13
     alert("you hit enter")
   }
 });
+
+$("h1").on("click", function() {
+  $(this).css("color", "purple");
+});
+
+$("input").on("keypress", function() {
+  console.log("keypressed");
+});
+
+$("button").on("mouseenter",function(){
+  // console.log("mouse enter!");
+  $(this).css("font-weight", "bold");
+})
+
+$("button").on("mouseleave",function(){
+  // console.log("mouse enter!");
+  $(this).css("font-weight", "normal");
+})
