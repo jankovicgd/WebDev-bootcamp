3ecf5e571dfab135502899fda9cf3d9ff0e3b009
jankovicgd <jankovic.gd@gmail.com>
2017-08-01 11:18:13 +0200

    more events

--- Section 17/events.js
+++ Section 17/events.js
@@ -1,9 +1,9 @@
 $('h1').click(function(){
-  alert("you clicked the h1")
+  alert("you clicked the h1");
 });
 
 $('button').click(function(){
-  alert("I told you not to click")
+  alert("I told you not to click");
 });
 
 $('#baton').click(function(){
@@ -12,4 +12,12 @@ $('#baton').click(function(){
 
 $('button').click(function(){
   console.log($(this).text());
+});
+
+$('input').keypress(function(event) {
+  //console.log("you pressed a key");
+  //console.log(event);
+  if (event.which == 13) {
+    alert("you hit enter")
+  }
 });
