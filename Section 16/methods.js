28cfcc56b892ed5cb0f68c889d55ea8af29323c9
jankovicgd <jankovic.gd@gmail.com>
2017-08-01 10:43:22 +0200

    methods

---
+++ Section 16/methods.js
@@ -0,0 +1,16 @@
+$('img').css('width', "400px");
+
+$('img:first-of-type').attr('src', 'https://www.commondreams.org/sites/default/files/imce-images/mad_dog.jpg');
+
+//$('input').attr('type', 'checkbox')
+
+$("input").val("Text input here");
+
+$('h1').addClass("correct");
+$('h1').removeClass("correct");
+
+$('li').addClass("correct");
+
+//$('li:first-of-type').addClass("done");
+
+//$('li').toggleClass('correct');
