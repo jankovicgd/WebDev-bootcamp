48f36c8e0149bbaefc90fd3b6cab6e3ed11dd715
jankovicgd <jankovic.gd@gmail.com>
2017-08-10 17:28:46 +0200

    Section 22, node js

---
+++ Section 22/DemoApp/app.js
@@ -0,0 +1,5 @@
+var cat = require("cat-me");
+var knocknock = require("knock-knock-jokes");
+
+console.log(cat('nyan'));
+console.log(knocknock());
---
+++ Section 22/echo.js
@@ -0,0 +1,8 @@
+function echo(string, times) {
+  for (var i = 0; i < times; i++) {
+    console.log(string);
+    }
+}
+
+echo("Echo!!!", 10);
+echo("Tater Tots", 3);
